17db4b523f71f64d84fa70f313131e7603dc6555
OmarAgamy9 <omaragamy@cmail.carleton.ca>
2022-04-07 15:43:50 -0400

    Added connection from the home page to surveyor

--- src/main/java/com/SurveyMonkey/controller/CreateQuestionController.java
+++ src/main/java/com/SurveyMonkey/controller/CreateQuestionController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CreateQuestionController {
 
+	@GetMapping("/createSurvey")
+	public String createSurvey() {
+		return "createSurvey";
+	}
+
 	@GetMapping("/createMCQuestion")
 	public String mcQuestion() {
 		return "createMCQuestion";
