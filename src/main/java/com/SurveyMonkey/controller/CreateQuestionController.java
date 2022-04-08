7720f3650640e2e746a18313e34e35e1ee3333a3
OmarAgamy9 <omaragamy@cmail.carleton.ca>
2022-04-07 20:55:27 -0400

    Created finished survey page

--- src/main/java/com/SurveyMonkey/controller/CreateQuestionController.java
+++ src/main/java/com/SurveyMonkey/controller/CreateQuestionController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CreateQuestionController {
 
+	@GetMapping("/finishCreate")
+	public String finishCreate() {
+		return "finishCreate";
+	}
+
+	@GetMapping("/homePage")
+	public String homePage() {
+		return "homePage";
+	}
+
 	@GetMapping("/createSurvey")
 	public String createSurvey() {
 		return "createSurvey";
